e11634497af1434371edf37bd363a2f62ef095fe
Mark McIver <mark@Marks-MacBook-Pro.local>
2019-02-25 12:49:59 -0700

    animations and transition lesson

--- src/App.js
+++ src/App.js
@@ -3,23 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+   
+    this.state = {
+      animationName: null,
+      navWidth: 0
+    };
+  }
+
+  toggleSpin = () => {
+    this.setState({
+      animationName: !this.state.animationName ? 'spin' : null
+    })
+  }
+
+  closeNav = () => {
+    this.setState({
+      navWidth: 0
+    })
+  }
+
+  openNav = () => {
+    this.setState({
+      navWidth: 250
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div id="mySidenav" className="sidenav" style={{width: this.state.navWidth}}>
+          <a href="#/" className="closebtn" onClick={this.closeNav}>&times;</a>
+          <a href="#/">About</a>
+          <a href="#/">Services</a>
+          <a href="#/">Clients</a>
+          <a href="#/">Contact</a>
+        </div>
+        <span style={{fontSize:30, cursor: 'pointer'}} onClick={this.openNav}>&#9776;</span>
+        {/* <div id="heart"></div> */}
+        {/* <div className="square" style={{animationName: this.state.animationName}} onClick={this.toggleSpin}></div> */}
+        {/* <MenuButton handleMouseDown={this.handleMouseDown} />
+        <Menu menuVisibility={this.state.visible} handleMouseDown={this.handleMouseDown} /> */}
       </div>
     );
   }
